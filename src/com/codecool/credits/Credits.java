720c678d398e2fe798c7fe9461ef0af9f23c1215
matyasczuczor <matyas.czuczor@gmail.com>
2019-06-06 22:25:29 +0200

    credits files added

---
+++ src/com/codecool/credits/Credits.java
@@ -0,0 +1,11 @@
+package com.codecool.credits;
+
+public class Credits {
+	
+	public static void credits() {
+		System.out.println("CREDITS\n");		
+		System.out.println("Dávid Liszkai\n");
+		System.out.println("Lóránd Fazekas\n");
+		System.out.println("Mátyás Czuczor\n");
+	}
+}
