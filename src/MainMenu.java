5f27ef0a222a841ad2dd35f1d130fcb9409e7ffa
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-06-07 02:15:14 +0200

    REFACTOR MainMenu, Table, ... etc

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,9 +3,9 @@ import com.codecool.currency.Currency;
 import static com.codecool.selecter.DataSelecter.simple;
 import static com.codecool.selecter.DataSelecter.detailed;
 import static com.codecool.selecter.DataSelecter.converted;
-import static com.codecool.display.showTable.printSimpleTable;
-import static com.codecool.display.showTable.printDetailedTable;
-import static com.codecool.display.showTable.printConvertedTable;
+import static com.codecool.display.Table.printSimpleTable;
+import static com.codecool.display.Table.printDetailedTable;
+import static com.codecool.display.Table.printConvertedTable;
 import static com.codecool.credits.Credits.credits;
 import java.io.IOException;
 
@@ -15,8 +15,11 @@ public class MainMenu {
     private static final String API_KEY_CRYPTOCURRENCY = "0081703480f82a60b595181a70323ac1065be2ed0aefa6ce538de6b8a24d6108";
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static Currency cryptoCurrency = new Currency();
+    private static List<Map<String, Object>> cryptos = cryptoCurrency.getCurrency(URL_CRYPTOCURRENCY,
+            API_KEY_CRYPTOCURRENCY);
 
-    public static void printMenu() {
+    public static void showMenu() {
         System.out.print("\033[H\033[2J");
         System.out.println("Stratton Oakmont CryptoCurrency Checker\n");
         System.out.println("Menu Options:");
@@ -28,52 +31,77 @@ public class MainMenu {
 
     }
 
-    public static void handleMenu() {
+    public static void startSimpleTable() {
+        
+        printSimpleTable(simple(cryptos));
+        
+        while (true) {
+            try {
+                int input = scanner.nextInt();
+                if (input >= 1 && input <= 10) {
+                    printDetailedTable(detailed(cryptos), input -1);
+                } else if (input ==0) {
+                    break;
+                } else {
+                    System.out.println("Try a number between range [1 - 10]\nOr press 0 to go Back");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Try a number please");
+                scanner.nextLine();
+            }
+
+        }
+    }
+
+    public static void startConvertedTable() {
+        printConvertedTable(converted(cryptos));
+        while (true) {
+            try {
+                int input = scanner.nextInt();
+                if (input == 0) {
+                    break;
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+
+    public static void chooseMenu() {
+        
         int input = scanner.nextInt();
         if (input < 0 || input > 5) {
             System.out.println("You have entered an invalid selection, please try again\n");
         } else if (input == 1) {
-            // System.out.print("\033[H\033[2J");
-            Currency cryptoCurrency = new Currency();
-            List<Map<String, Object>> cryptos = cryptoCurrency.getCurrency(URL_CRYPTOCURRENCY, API_KEY_CRYPTOCURRENCY);
-
-            String line = "";
-            printSimpleTable(simple(cryptos));
-            System.out.println("Choose a Cryptocurrency number to detailed view");
-            System.out.println("Press 'q' to Main Menu");
-
-            while (line.equalsIgnoreCase("q") == false) {
-
-                if (line.equalsIgnoreCase("5") == true) {
-                    printDetailedTable(detailed(cryptos), 5);
-                }
-                line = scanner.nextLine();
-                // System.out.print("\033[H\033[2J");
-
-            }
-
-            printMenu();
-            handleMenu();
-
+            startSimpleTable();
         } else if (input == 2) {
-            Currency cryptoCurrency = new Currency();
-            List<Map<String, Object>> cryptos = cryptoCurrency.getCurrency(URL_CRYPTOCURRENCY, API_KEY_CRYPTOCURRENCY);
-            printConvertedTable(converted(cryptos));
+            // System.out.print("\033[H\033[2J");
+            // printConvertedTable(converted(cryptos));
+            startConvertedTable();
         } else if (input == 3) {
-            System.out.print("\033[H\033[2J");
+            // System.out.print("\033[H\033[2J");
             credits();
         } else if (input == 4) {
             System.out.println("You have quit the program\n");
             System.exit(1);
         } else {
-            System.out.println("You have entered " + input + "\n");
+            System.out.println("error");
+            
+            throw new InputMismatchException();
         }
     }
 
     public static void main(String[] args) throws IOException {
 
-        printMenu();
-        handleMenu();
+        while (true) {
+            showMenu();
+            try {
+                chooseMenu();
+            } catch (InputMismatchException e) {
+                System.out.println("Try a number please");
+                scanner.next();
+            }
 
+        }
     }
 }
