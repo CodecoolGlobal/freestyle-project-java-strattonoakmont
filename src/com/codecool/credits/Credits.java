6d3ca9ac7f36699a426e0cf9564998477f09b50f
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-06-07 05:48:20 +0200

    Final Presentation Version 1.0

--- src/com/codecool/credits/Credits.java
+++ src/com/codecool/credits/Credits.java
@@ -5,7 +5,7 @@ public class Credits {
 	public static void showCredits() {
 		System.out.println("CREDITS\n");		
 		System.out.println("Dávid Liszkai\n");
-		System.out.println("Lóránd Fazekas\n");
+		System.out.println("Lóránd György-Fazakas\n");
 		System.out.println("Mátyás Czuczor\n");
 	}
 }
